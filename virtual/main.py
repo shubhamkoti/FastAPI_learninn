2649e0b671c9c4b06b8dda0fa4865e78a5ce57c1
shunbham koti <shubhamkoti81@gmail.com>
2026-07-02 11:47:55 +0530

    Dynamic Routes

--- virtual/main.py
+++ virtual/main.py
@@ -2,23 +2,8 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get('/')
-def home():
-  return {"message":"this is About page"}
 
-@app.get("/about")
-def about():
-  return {"Message" :"this is about page"}
+@app.get("/users/{user_id}")
+def user(user_id):
+  return {"users":user_id}
 
-@app.get("/users")
-def user():
-  return {"users":["Mohit","shubh"]}
-
-
-@app.get("/login")
-def login():
-  return {
-    "name":"javir",
-    "age":24,
-    "Skills":["C++","Python"]
-  }
